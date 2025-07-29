d30a275ad1e144daf526d4d78ff1431128dbb4a2
Salvador <salvadorabbott11@gmail.com>
2025-07-29 11:48:06 +0200

    Add animals

--- exam-java/src/main/java/com/cdsb/AppZoo.java
+++ exam-java/src/main/java/com/cdsb/AppZoo.java
@@ -1,10 +1,14 @@
 package com.cdsb;
 
+import com.cdsb.Class.Aquatics.Dolphin;
+import com.cdsb.Class.Aquatics.Seal;
 import com.cdsb.Class.Birds.Parrot;
 import com.cdsb.Class.Birds.Toucan;
 import com.cdsb.Class.Mammals.Elephant;
 import com.cdsb.Class.Mammals.Lion;
 import com.cdsb.Class.Mammals.Zebra;
+import com.cdsb.Class.Reptiles.Iguana;
+import com.cdsb.Class.Reptiles.Snake;
 import com.cdsb.Enums.Behaviour;
 import com.cdsb.Enums.Feed;
 import com.cdsb.Enums.Habitats;
@@ -18,7 +22,28 @@ public class AppZoo {
         Zebra z= new Zebra(Behaviour.PREY, Habitats.TERRESTRIAL, 40, Feed.HERBIVOROUS, 3, true,Species.MAMMAL);
         Parrot p = new Parrot(Behaviour.PREY, Habitats.TERRESTRIAL, 10,Feed.OMNIVOROUS, 5, false, Species.BIRDS);
         Toucan t = new Toucan(Behaviour.PREY, Habitats.TERRESTRIAL, 6, Feed.OMNIVOROUS, 2, false, Species.BIRDS);
-        
+        Snake sn= new Snake(Behaviour.PREDATOR,Habitats.TERRARIUM,20,Feed.CARNIVOROUS,1,true,Species.REPTIL);
+        Iguana i= new Iguana(Behaviour.PREDATOR,Habitats.TERRARIUM, 34, Feed.CARNIVOROUS, 2, false,Species.REPTIL);
+        Dolphin d= new Dolphin(Behaviour.PREDATOR, Habitats.AQUATIC, 40, Feed.CARNIVOROUS, 3, false, Species.ACUATIC);
+        Seal sl= new Seal(Behaviour.PREDATOR, Habitats.AQUATIC, 15, Feed.CARNIVOROUS, 2, true, Species.ACUATIC);
+        l.showAnimal();
+        System.out.println("--------");
+        e.showAnimal();
+        System.out.println("--------");
+        z.showAnimal();
+        System.out.println("--------");
+        p.showAnimal();
+        System.out.println("--------");
+        t.showAnimal();
+        System.out.println("--------");
+        sn.showAnimal();
+        System.out.println("--------");
+        i.showAnimal();
+        System.out.println("--------");
+        d.showAnimal();
+        System.out.println("--------");
+        sl.showAnimal();
+
     }
 
 }
